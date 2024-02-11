6c6a45dea3a666b0b44049ce1aced7d39a6e5d5c
ShiroTohu <shirotohu@shirotohu.dev>
2024-02-11 11:45:59 +1000

    + Migrated to Nextra

---
+++ theme.config.jsx
@@ -0,0 +1,9 @@
+export default {
+    logo: <span>CCStorageManagement</span>,
+    footer: {
+        text: `Copyright (c) ${(new Date()).getFullYear()}, shirotohu | All rights reserved`
+    },
+    project: {
+        link: 'https://github.com/ShiroTohu/CCStorageManagement.Client'
+    }
+}
